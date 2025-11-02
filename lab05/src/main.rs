202432d2f475974526268cff190d6813f861271a
edia8 <eaioanei14@gmail.com>
2025-11-02 22:50:35 +0200

    lab05 done

---
+++ lab05/src/main.rs
@@ -0,0 +1,249 @@
+use serde_derive::Deserialize;
+use std::fs::File;
+use std::io::{self, BufRead};
+//P1
+#[derive(Debug, Clone, Deserialize)]
+struct Student {
+    name: String,
+    phone_number: String,
+    age: u8,
+}
+
+fn read_from_file(path: &str) -> Result<Vec<Student>, std::io::Error> {
+    let file = File::open(path)?; //.expect("Failed to open file");
+    let citit = io::BufReader::new(file);
+    let mut students = Vec::new();
+    for line in citit.lines() {
+        let line = line?;
+        let obj: Vec<&str> = line.split(',').collect();
+        let name = obj[0].to_string();
+        let phone_number = obj[1].to_string();
+        let age = obj[2].parse().unwrap_or(0);
+        students.push(Student {
+            name,
+            phone_number,
+            age,
+        });
+    }
+    Ok(students)
+}
+
+fn read_from_json(path: &str) -> Result<Vec<Student>, std::io::Error> {
+    let file = File::open(path)?;
+    let citit = io::BufReader::new(file);
+    let mut students = Vec::new();
+    for s in citit.lines() {
+        let s = s?;
+        match serde_json::from_str(&s) {
+            Ok(stud) => {
+                students.push(stud);
+            }
+            Err(_) => println!("Nu s-a citit studentul actual"),
+        }
+    }
+    Ok(students)
+}
+
+//p2
+struct Canvas {
+    canvas: [[char; 100]; 55],
+}
+
+fn new_canvas() -> Canvas {
+    Canvas {
+        canvas: [[' '; 100]; 55],
+    }
+}
+fn set_pixels(c: &mut Canvas, pixels: &[(usize, usize, u8)]) {
+    for (x, y, ch) in pixels {
+        c.canvas[*x][*y] = *ch as char;
+    }
+}
+
+fn draw(c: &mut Canvas) {
+    set_pixels(c, &[(4, 25, 124), (3, 33, 124), (2, 24, 95), (4, 3, 95)]);
+    set_pixels(c, &[(7, 2, 95), (4, 21, 124), (5, 16, 95)]);
+    set_pixels(c, &[(4, 41, 124), (7, 1, 124), (5, 8, 92)]);
+    set_pixels(c, &[(1, 31, 40), (2, 3, 95), (2, 41, 124)]);
+    set_pixels(
+        c,
+        &[
+            (2, 16, 95),
+            (5, 35, 92),
+            (6, 3, 95),
+            (2, 11, 95),
+            (5, 3, 95),
+        ],
+    );
+    set_pixels(
+        c,
+        &[
+            (2, 38, 95),
+            (4, 9, 40),
+            (3, 41, 124),
+            (2, 37, 95),
+            (2, 25, 124),
+        ],
+    );
+    set_pixels(
+        c,
+        &[
+            (5, 27, 124),
+            (2, 27, 124),
+            (4, 0, 124),
+            (3, 35, 47),
+            (2, 18, 95),
+        ],
+    );
+    set_pixels(c, &[(4, 13, 124), (4, 37, 95), (4, 16, 40), (3, 6, 124)]);
+    set_pixels(c, &[(7, 32, 47), (4, 20, 124), (5, 11, 95), (5, 42, 95)]);
+    set_pixels(c, &[(5, 15, 92), (4, 34, 124), (4, 45, 41), (5, 24, 95)]);
+    set_pixels(c, &[(4, 2, 40), (7, 3, 95), (2, 44, 95)]);
+    set_pixels(
+        c,
+        &[
+            (6, 30, 95),
+            (5, 45, 95),
+            (4, 31, 124),
+            (4, 7, 124),
+            (3, 43, 39),
+        ],
+    );
+    set_pixels(c, &[(5, 17, 95), (1, 27, 124), (2, 5, 95)]);
+    set_pixels(
+        c,
+        &[
+            (3, 44, 95),
+            (3, 19, 92),
+            (5, 23, 95),
+            (3, 8, 47),
+            (2, 10, 95),
+        ],
+    );
+    set_pixels(c, &[(6, 6, 124), (5, 19, 47), (3, 24, 95), (3, 27, 124)]);
+    set_pixels(
+        c,
+        &[
+            (3, 10, 95),
+            (4, 44, 95),
+            (2, 9, 95),
+            (0, 32, 95),
+            (5, 2, 95),
+        ],
+    );
+    set_pixels(c, &[(6, 2, 95), (7, 31, 95), (1, 25, 124), (2, 36, 95)]);
+    set_pixels(
+        c,
+        &[
+            (3, 46, 92),
+            (5, 25, 44),
+            (1, 43, 124),
+            (5, 46, 47),
+            (3, 15, 47),
+        ],
+    );
+    set_pixels(c, &[(4, 17, 95), (2, 23, 95), (3, 39, 92)]);
+    set_pixels(c, &[(4, 47, 124), (2, 45, 95), (3, 37, 95)]);
+    set_pixels(
+        c,
+        &[
+            (5, 44, 95),
+            (2, 2, 95),
+            (5, 10, 95),
+            (5, 9, 95),
+            (4, 43, 124),
+        ],
+    );
+    set_pixels(c, &[(4, 38, 41), (2, 17, 95), (0, 26, 95)]);
+    set_pixels(c, &[(4, 18, 41), (7, 5, 47), (5, 41, 124), (5, 33, 124)]);
+    set_pixels(c, &[(5, 12, 47), (5, 22, 92), (6, 33, 124), (5, 31, 124)]);
+    set_pixels(
+        c,
+        &[
+            (4, 40, 124),
+            (3, 3, 95),
+            (4, 4, 124),
+            (6, 31, 47),
+            (3, 4, 96),
+        ],
+    );
+    set_pixels(c, &[(0, 42, 95), (5, 18, 95), (4, 27, 124)]);
+    set_pixels(
+        c,
+        &[
+            (3, 12, 92),
+            (2, 32, 95),
+            (5, 37, 95),
+            (5, 26, 95),
+            (5, 39, 47),
+        ],
+    );
+    set_pixels(c, &[(3, 25, 96), (4, 14, 124), (4, 33, 124), (3, 1, 47)]);
+    set_pixels(
+        c,
+        &[
+            (5, 36, 95),
+            (7, 30, 95),
+            (6, 4, 47),
+            (4, 24, 95),
+            (1, 32, 95),
+        ],
+    );
+    set_pixels(c, &[(3, 22, 47), (4, 23, 40), (5, 6, 124)]);
+    set_pixels(c, &[(1, 33, 41), (1, 41, 124), (7, 29, 124)]);
+    set_pixels(c, &[(4, 6, 124), (5, 38, 95), (3, 31, 124), (7, 4, 95)]);
+    set_pixels(c, &[(4, 11, 41), (4, 10, 95), (5, 1, 92)]);
+    set_pixels(c, &[(2, 43, 124), (3, 17, 95), (5, 4, 44), (4, 36, 40)]);
+    set_pixels(c, &[(5, 43, 46)]);
+}
+
+fn print(c: Canvas) {
+    let mut ok;
+    'a: for i in 0..55 {
+        ok = false;
+        for j in 0..100 {
+            print!("{}", c.canvas[i][j]);
+            ok = (c.canvas[i][j] != ' ') || ok;
+        }
+        if !ok {
+            break 'a;
+        }
+        println!();
+    }
+}
+fn old_young_student(s: &[Student]) -> (Student, Student) {
+    let mut olds: Student = s[0].clone();
+    let mut youngs: Student = s[0].clone();
+    for student in s.iter() {
+        if student.age > olds.age {
+            olds.age = student.age;
+            olds.name = student.name.clone();
+            olds.phone_number = student.phone_number.clone();
+        }
+        if student.age < youngs.age {
+            youngs.age = student.age;
+            youngs.name = student.name.clone();
+            youngs.phone_number = student.phone_number.clone();
+        }
+    }
+    (olds, youngs)
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let path = "inputp1.txt";
+    let s: Vec<Student> = read_from_file(path)?;
+    let (olds, youngs) = old_young_student(&s);
+    println!("P1\nBatran: {olds:#?}\n Tanar: {youngs:#?}");
+
+    println!("\nP2:");
+    let mut canvas = new_canvas();
+    draw(&mut canvas);
+    print(canvas);
+
+    println!("\nP3:");
+    let path = "input.jsonl";
+    let st = read_from_json(path)?;
+    let (oldst, youngst) = old_young_student(&st);
+    println!("Batran: {oldst:#?}\n Tanar: {youngst:#?}");
+    Ok(())
+}
