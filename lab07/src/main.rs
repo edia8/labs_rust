8b571dc7d5e7410bca08f75c72d262e5d8a39204
edia8 <eaioanei14@gmail.com>
2025-11-14 11:26:01 +0200

    lab07 done

--- lab07/src/main.rs
+++ lab07/src/main.rs
@@ -12,10 +12,11 @@ macro_rules! assert_eq_rel {
     };
 }
 
+use core::fmt;
 use std::fmt::Debug;
-use std::ops::{Add,Sub,Mul};
+use std::ops::{Add,Sub,Mul,Neg,AddAssign,SubAssign,MulAssign};
 use std::cmp::PartialEq;
-#[derive(Clone, Copy)]
+#[derive(Debug,Clone, Copy)]
 struct Complex {
     real:f64,
     imag:f64,
@@ -33,44 +34,108 @@ impl Complex {
         Self { real: self.real, imag: -self.imag }
     }
 }
-impl Add for Complex {
-    type Output = Self;
-    fn add(self, param: Self) -> Self::Output {
+impl Default for Complex {
+    fn default() -> Self {
+        Complex { real: 0f64, imag: 0f64 }
+    }
+}
+impl From<i32> for Complex {
+    fn from(value: i32) -> Self {
+        Complex { real: value as f64, imag: 0f64 }
+    }
+}
+impl From<f64> for Complex {
+    fn from(value: f64) -> Self {
+        Complex { real: value, imag: 0f64 }
+    }
+}
+
+impl<T> Add<T> for Complex 
+where 
+    Complex:From<T>{
+    type Output = Complex;
+    fn add(self, param: T) -> Self::Output {
+        let param = Complex::from(param);
         Self {
             real : self.real + param.real,
             imag : self.imag + param.imag,
         }
     }
 }
-impl Sub for Complex {
+impl<T> Sub<T> for Complex 
+where 
+    Complex:From<T>{
     type Output = Self;
-    fn sub(self, param: Self) -> Self::Output {
+    fn sub(self, param: T) -> Self::Output {
+        let param = Complex::from(param);
         Self {
             real : self.real - param.real,
             imag : self.imag - param.imag,
         }
     }
 }
-impl Mul for Complex {
+impl<T> Mul<T> for Complex 
+where
+    Complex:From<T>{
     type Output = Self;
-    fn mul(self, param: Self) -> Self::Output {
+    fn mul(self, param: T) -> Self::Output {
+        let param = Complex::from(param);
         Self {
             real : self.real * param.real - self.imag * param.imag,
             imag : self.real * param.imag + self.imag * param.real,
         }
     }
 }
+impl Neg for Complex {
+    type Output = Complex;
+    fn neg(self) -> Self::Output {
+        Complex{real:-self.real,imag:-self.imag}
+    }
+}
+impl<T> AddAssign<T> for Complex
+where
+    Complex:From<T> {
+    fn add_assign(&mut self, rhs: T) {
+        let rhs = Complex::from(rhs);
+        self.real += rhs.real;
+        self.imag += rhs.imag;
+    }
+}
+impl<T> SubAssign<T> for Complex
+where
+    Complex:From<T> {
+        fn sub_assign(&mut self, rhs: T) {
+            let rhs = Complex::from(rhs);
+            self.real -= rhs.real;
+            self.imag -= rhs.imag;
+        }
+    }
+impl<T> MulAssign<T> for Complex
+where
+    Complex:From<T> {
+        fn mul_assign(&mut self, rhs: T) {
+            let rhs = Complex::from(rhs);
+            let rnew = self.real * rhs.real - self.imag * rhs.imag;
+            let inew = self.real * rhs.imag + self.imag * rhs.real;
+            *self = Complex::new(rnew, inew);
+        }
+    }
 impl PartialEq for Complex {
     fn eq(&self, other: &Self) -> bool {
         self.real == other.real && self.imag == other.imag
     }
+    fn ne(&self, other: &Self) -> bool {
+        !self.eq(other)
+    }
 }
-impl Debug for Complex {
+impl fmt::Display for Complex {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if self.imag > 0f64 {
-            f.write_fmt(format_args!("{}+{}i",self.real,self.imag))
-        } else {
-            f.write_fmt(format_args!("{}{}i",self.real,self.imag))
+        match self {
+            Complex{real:0f64,imag:0f64} => write!(f,"0"),
+            Complex{real,imag:0f64} => f.write_fmt(format_args!("{}",*real)),
+            Complex{real:0f64,imag} => f.write_fmt(format_args!("{}i",*imag)),
+            Complex{real,imag} if self.imag > 0f64 => f.write_fmt(format_args!("{}+{}i",*real,*imag)),
+            Complex{real,imag} => f.write_fmt(format_args!("{}{}i",*real,*imag))
         }
     }
 }
@@ -93,5 +158,20 @@ fn main() {
 
     let f = (a + b - d) * c;
     assert_eq!(f, Complex::new(-7, 11));
-    println!("ok");
+
+    // Note: .to_string() uses Display to format the type
+    assert_eq!(Complex::new(1, 2).to_string(), "1+2i");
+    assert_eq!(Complex::new(1, -2).to_string(), "1-2i");
+    assert_eq!(Complex::new(0, 5).to_string(), "5i");
+    assert_eq!(Complex::new(7, 0).to_string(), "7");
+    assert_eq!(Complex::new(0, 0).to_string(), "0");
+
+    let h = Complex::new(-4, -5);
+    let i = h - (h + 5) * 2.0;
+    assert_eq_rel!(i.real, -6);
+
+    let j = -i + i;
+    assert_eq_rel!(j.real, 0);
+    assert_eq_rel!(j.imag, 0);
+    println!("ok!");
 }
